507c5db0870e19d46196c957168aa7dac76c8e5d
Wh1plash77 <Pupies1332@gmail.com>
2021-04-09 19:34:58 -0400

    Nav-Mesh Plane implementation
    The "plane" can now follow a series of points (empty game objects that are children of a Point object) at a set speed on an invisible Nav Mesh surface

---
+++ FloorIsLava/Assets/Prefabs/Plane/NetworkNavMeshAgent.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkNavMeshAgent : MonoBehaviour
+{
+	public UnityEngine.AI.NavMeshAgent MyAgent;
+	//float XCord = 30;
+	Vector3 InitPos;
+	private NetworkTransform MyNetT;
+	public PlaneControlScript plane;
+
+	public GameObject Points;
+	public int destPoint = 1;
+
+	void Start()
+	{
+		InitPos = this.transform.position;
+		MyAgent = this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+		MyNetT = GetComponent<NetworkTransform>();
+		MyAgent.autoBraking = false;
+
+		Points = GameObject.Find("Points");
+
+		plane = GetComponent<PlaneControlScript>();
+
+		//MyAgent.baseOffset = 15f;
+
+		StartCoroutine(SlowStart());
+	}
+
+	public IEnumerator SlowStart()
+	{
+		yield return new WaitUntil(() => MyNetT.MyNetId.NetId != -10);
+		//Debug.Log("Got valid NetId");
+		MyNetT.UsingNavMesh = MyNetT.MyNetId.NetId % 2 == 0;
+		yield return new WaitUntil(() => MyAgent.isOnNavMesh);
+		if (MyNetT.IsServer)
+		{
+			InitPos = this.transform.position;
+			//Debug
+
+			MyAgent.isStopped = false;
+		}
+	}
+
+	void Update()
+	{
+		if (MyAgent.remainingDistance < .1f && MyAgent.isOnNavMesh)
+		{
+			GoToNext();
+		}
+	}
+
+	void GoToNext()
+	{
+		if(destPoint >= 7)
+        {
+			destPoint = 1;
+        }
+        else
+        {
+			destPoint += 1;
+        }
+
+		plane.PointChange(destPoint);
+	}
+}
